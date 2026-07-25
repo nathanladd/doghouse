4132d92c0e0a08e5899461cb136a0f0d427cec8e
Nate Ladd <62190822+nathanladd@users.noreply.github.com>
2026-07-25 17:29:12 -0600

    Added some component check lines

--- bobcat-ac/assets/js/main.js
+++ bobcat-ac/assets/js/main.js
@@ -87,7 +87,7 @@ function buildHeader() {
     : 'Service Training';
 
   header.innerHTML = `
-    <div class="logo"><a href="${courseHref}" style="text-decoration: none; color: inherit;">Bobcat AC: <span>Making Stuff Less Hot</span></a></div>
+    <div class="logo"><a href="${courseHref}" style="text-decoration: none; color: inherit;">AC: <span>Making Stuff Less Hot</span></a></div>
     <div class="breadcrumb">${breadcrumbHtml}</div>
     <div class="header-search" role="search">
       <input type="search" class="header-search-input" placeholder="Search this course&hellip;" aria-label="Search the Bobcat AC course" autocomplete="off" spellcheck="false">
